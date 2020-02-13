330665bfe8ef5941844c677ebf9b5f78e69d0ebe
Scott <scottgeilen99@gmail.com>
2020-02-13 17:44:29 -0500

    adjusted HourlyRate()

--- Veterinarian.java
+++ Veterinarian.java
@@ -1,18 +1,10 @@
 package hw3;
 import java.util;
-public class Veterinarian implements HW3Interface { 
+public class Veterinarian extends Zookeeper implements HW3Interface { 
     String id;   
     int money;
-    public void Individual(String newName) {
-        id = newName;
-    }
-    public void Address(String newName) {
-        id = newName;
-    }
-    public void Specialty(String newName) {
-        id = newName;
-    }
-    public void HourlyRate(int Wage) {
+    private int HourlyRate(int Wage) {
         money = Wage;
+        return Wage;
     }
 }
