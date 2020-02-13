d032f2194cf516f473d66e79e70e91dfcc8bff73
Scott <scottgeilen99@gmail.com>
2020-02-13 18:29:38 -0500

    fixed bugs

--- Zoo.java
+++ Zoo.java
@@ -1,11 +1,11 @@
 package hw3;
 import java.util;
-public interface Zoo implements HW3Interface {
+public interface Zoo extends HW3Interface {
     public static void main(String args[]) {
         Enclosure();
         Zookeeper();
         Veterinarian();
         Animal();
-        AnimalGroup;
+        AnimalGroup();
     }
 }
